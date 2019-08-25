74485272be228dabd0ab1fab20ad6fbb42a9a3df
Lindsay Preston <lindsayarthur303@gmail.com>
2019-08-25 16:56:05 -0600

    click

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => (
+  <div className="jumbotron jumbotron-fluid text-center">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 offset-md-2 offset-md-right-2">
+          <h1>Clicky Game!</h1>
+          <h4>Click on an image to earn points, don't click any one twice!</h4>
+        </div>
+    </div>
+    </div>
+  </div>
+);
+
+export default Banner;
